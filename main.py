6ae9d216cede498f405045883cb135436698020f
dmitrii klochkov <d.klochkov9421@gmail.com>
2023-02-12 23:12:58 +0300

    обавил код на распознавание лица по веб камере

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Тут мы рассматриваем пример увеличения фотографии или обьекта изображения.
 import cv2
 
-img = cv2.imread('Your image here')
+img = cv2.imread('C:\\Users\dkloc\Downloads\Telegram Desktop\Dima.jpg')
 print(img.shape)
 
 img = cv2.resize(img, (1000, 1000))
--- video.py
+++ video.py
@@ -1,14 +1,29 @@
 # В этом примере рассматривает взаимодействие в вебкамерой.
+# Все работает
 import cv2
 
-face_Cascades = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascades_frontalface_default.xml")
+face_cascades = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+# img = cv2.imread("C:\\Users\dkloc\Downloads\Telegram Desktop\_NIK2533.png")
+# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+# for (x, y, w, h) in faces:
+# cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+# cv2.imshow("Result", img_gray)
+
+# cv2.waitKey(0)
 
 cap = cv2.VideoCapture(0)
 
 while True:
     success, frame = cap.read()
-    cv2.imshow("Camera, Q exit program", frame)
-
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascades.detectMultiScale(img_gray)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.imshow("Result", frame)
     if cv2.waitKey(1) & 0xff == ord("q"):
         break
